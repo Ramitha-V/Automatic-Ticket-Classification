938ac9b7dd49caeb1791e87c54689326524f3b26
Ramitha-V <162662008+Ramitha-V@users.noreply.github.com>
2024-05-15 13:45:46 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,102 @@
+from sqlalchemy import create_engine, text
+import os
+from datetime import date
+
+current_date = date.today()
+
+connection_info = os.environ['connection_info']
+
+engine = create_engine(connection_info,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+def load_tickets_from_db_user():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from properly_labeled ORDER BY `date` DESC LIMIT 6"))
+    tickets = []
+    for row in result.all():
+      tickets.append(dict(row._asdict()))
+  return tickets
+
+
+def load_tickets_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from properly_labeled ORDER BY `date` DESC"))
+    tickets = []
+    for row in result.all():
+      tickets.append(dict(row._asdict()))
+  return tickets
+
+
+def load_tickets_0():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from properly_labeled where Dominant_Topic=0 ORDER BY `date` DESC"))
+    tickets = []
+    for row in result.all():
+      tickets.append(dict(row._asdict()))
+  return tickets
+
+def load_tickets_1():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from properly_labeled where Dominant_Topic=1 ORDER BY `date` DESC"))
+    tickets = []
+    for row in result.all():
+      tickets.append(dict(row._asdict()))
+  return tickets
+
+def load_tickets_2():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from properly_labeled where Dominant_Topic=2 ORDER BY `date` DESC"))
+    tickets = []
+    for row in result.all():
+      tickets.append(dict(row._asdict()))
+  return tickets
+
+def load_tickets_3():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from properly_labeled where Dominant_Topic=3 ORDER BY `date` DESC"))
+    tickets = []
+    for row in result.all():
+      tickets.append(dict(row._asdict()))
+  return tickets
+
+def load_tickets_4():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from properly_labeled where Dominant_Topic=4 ORDER BY `date` DESC"))
+  tickets = []
+  for row in result.all():
+    tickets.append(dict(row._asdict()))
+  return tickets
+
+
+def add_ticket_to_db(data, topic):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO properly_labeled (zip_code, issue, date, state, product, company, complaint_what_happened, tags, Dominant_Topic) VALUES(:zip_code, :issue, :date, :state, :product, :company, :complaint_what_happened, :tags, :topic)"
+    )
+    variables = {
+      "zip_code": data['zip_code'],
+      "issue": data['issue'],
+      "date": current_date,
+      "state": data['state'],
+      "product": data['product'],
+      "company": data['company'],
+      "complaint_what_happened": data['complaint_what_happened'],
+      "tags": data['tags'],
+      "topic": topic
+    }
+    conn.execute(query, variables)
+
+def load_one_complaint_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from properly_labeled where complaint_id = :val"), {"val":id})
+    row=result.fetchall()
+  return dict(row[0]._asdict())
